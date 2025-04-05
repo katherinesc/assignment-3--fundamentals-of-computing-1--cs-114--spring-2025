ea153d30326ee43ac059517ed5b92ad9c6e3cc50
katherinesc <kcorrea10506@gmail.com>
2025-04-05 14:19:13 -0400

    Asked user for diamond size

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    int sizeOfDiamond;
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter a number: ");
+    sizeOfDiamond = scan.nextInt();
+
   }
 }
