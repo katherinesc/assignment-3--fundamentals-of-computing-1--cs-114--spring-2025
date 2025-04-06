f4067ec38a4cddc5a764c417e8c6c5182364e0f6
katherinesc <kcorrea10506@gmail.com>
2025-04-06 18:24:36 -0400

    Half of odd diamond

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -11,8 +11,34 @@ public class Diamond {
 
     if(sizeOfDiamond % 2 == 0){
       System.err.println("Number is even");
-    } else{
-      System.out.println("Number is odd");
+    }
+
+    else {
+      System.out.print("Number is odd");
+      for(int creationOfDiamond = 1; creationOfDiamond <= sizeOfDiamond; creationOfDiamond++){
+        for(int spaces = 1; spaces <= sizeOfDiamond - creationOfDiamond; spaces++){
+          System.err.print(" ");
+        }
+
+        for(int diamondCharacters = 1; diamondCharacters <= creationOfDiamond - 1 * 2 ; diamondCharacters++){
+          System.out.print("*");
+        }
+
+        System.out.println();
+      }
+
+      for (int creationOfDiamond = sizeOfDiamond - 1; creationOfDiamond > 0; creationOfDiamond--){
+        for(int spaces = 1; spaces <= sizeOfDiamond - creationOfDiamond; spaces++){
+          System.out.print(" ");
+        }
+
+        for(int diamondCharacters = 1; diamondCharacters <= creationOfDiamond - 1 * 2; diamondCharacters++){
+          System.out.print("*");
+        }
+
+        System.out.println();
+      }
+
     }
 
   }
