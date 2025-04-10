7eaeab6fda834665663b659568c430a826473106
katherinesc <kcorrea10506@gmail.com>
2025-04-10 01:05:30 -0400

    Swap method

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -1,4 +1,9 @@
 public class Matrix {
   private int[][] matrix;
 
+  private void swap(int x1, int y1, int x2, int y2){
+    int temp = matrix[x1][y1];
+    matrix[x1][y1] = matrix[x2][y2];
+    matrix[x2][y2] = temp;
+  }
 }
