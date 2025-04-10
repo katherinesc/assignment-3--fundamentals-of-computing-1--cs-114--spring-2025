89ad244f0b98f3a2aa3f17c64ebabb90b6a2e41b
katherinesc <kcorrea10506@gmail.com>
2025-04-10 05:05:53 -0400

    Matrix method

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -1,6 +1,8 @@
 public class Matrix {
   private int[][] matrix;
 
+  public Matrix(int size){
+    matrix = new int[size][size];
   private void swap(int x1, int y1, int x2, int y2){
     int temp = matrix[x1][y1];
     matrix[x1][y1] = matrix[x2][y2];
