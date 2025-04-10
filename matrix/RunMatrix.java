0b5ec9bf21cb8d3fc8ebb9a76e10427784003cea
katherinesc <kcorrea10506@gmail.com>
2025-04-10 05:00:30 -0400

    Labeling colors

--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -2,5 +2,8 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    final String RESET = "\u001b[0m";
+    final String HIGHLIGHT = "\u001b[33m";
+
   }
 }
