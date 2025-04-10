dba406f5e0c7e227175881f3df74766f15b416cf
katherinesc <kcorrea10506@gmail.com>
2025-04-10 05:01:54 -0400

    Loop to print 0s matrix

--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -16,5 +16,15 @@ public class RunMatrix {
     System.out.println();
     System.out.println("Here is the matrix with deafult values: ");
 
+    for(int row = 0; row < size; row++){
+      for(int column = 0; column < size; column++){
+        if(row + column == size - 1){
+          System.out.print(HIGHLIGHT + "0\t" + RESET);
+        }
+        System.out.print("0\t");
+      }
+      System.out.println();
+    }
+
   }
 }
