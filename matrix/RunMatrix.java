6a5fd36bd8c5d093e4b1639d8dbddbc7d1ccce7e
katherinesc <kcorrea10506@gmail.com>
2025-04-10 05:01:05 -0400

    Getting user input and using it

--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -5,5 +5,11 @@ public class RunMatrix {
     final String RESET = "\u001b[0m";
     final String HIGHLIGHT = "\u001b[33m";
 
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter a positive number for the size of your matrix: ");
+    size = scan.nextInt();
+
+    Matrix matrix = new Matrix(size);
   }
 }
