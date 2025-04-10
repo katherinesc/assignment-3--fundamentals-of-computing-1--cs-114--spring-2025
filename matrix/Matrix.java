1b36bc6d976ae57e3a48215aa75b62cafb6c6c7e
katherinesc <kcorrea10506@gmail.com>
2025-04-10 01:06:04 -0400

    Printmatrix method

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -6,4 +6,21 @@ public class Matrix {
     matrix[x1][y1] = matrix[x2][y2];
     matrix[x2][y2] = temp;
   }
+  public void printMatrix(){
+    final String RESET = "\u001b[0m";
+    final String HIGHLIGHT = "\u001b[33m";
+
+    int matrixLength = matrix.length;
+    for(int row = 0; row < matrixLength; row++ ){
+      for(int column = 0; column < row; column++){
+        if(column == matrixLength - 1 - row){
+          System.out.println(HIGHLIGHT + matrix[row][column] + RESET);
+        }
+        else {
+          System.out.println(matrix[row][column]);
+        }
+      }
+      System.out.println();
+    }
+  }
 }
